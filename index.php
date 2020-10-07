6f03883c872a4b5505a5b2c417a3e58ff9683860
AngJunJiat <49503798+AngJunJiat@users.noreply.github.com>
2020-10-07 16:13:09 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,320 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr" id="close-modal">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ITOPS</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./css/foundation.min.css">
+    <link rel="stylesheet" href="./css/animate__4.css">
+    <link rel="stylesheet" href="./css/slick.css">
+    <link rel="stylesheet" href="./css/slick-theme.css">
+    <link rel="stylesheet" href="./css/fontawesome.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.9/fullpage.min.css">
+    <link rel="stylesheet" href="./css/main.min.css?v.<?php echo time();?>">
+    <link rel="stylesheet" href="./css/app.css?v.<?php echo time();?>">
+</head>
+
+<body>
+	<?php include('header.php');?>
+		
+	<div id="fullPage">
+		<section class="s1">
+			<div class="mobile-banner-img"></div>
+			<div class="header-banner">
+				<div class="grid-container full">
+					<div class="grid-x grid-paddind-x">
+						<div class="large-12 cell">
+							<div class="banner-title">
+								<h1>Enhanced<br>Event</br>Management</h1>
+								<p>Manage your IT infrastructure securely and<br>effectively with our powerful automation tools for<br>optimized computing and performance.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		<section class="s1">
+			<div class="sec2-wrap" style="background-color: #001B47;">
+				<div class="fix-solution-padding">
+					<div class="grid-container full">
+						<div class="grid-x grid-padding-x" data-equalizer>
+							<div class="large-4 medium-12 small-12 cell">
+								<div class="fix-solution-banner" data-equalizer-watch></div>
+							</div>
+		
+							<div class="large-8 medium-12 small-12 cell">
+								<div class="fix-solution-title" data-equalizer-watch>
+									<h3>Beyond A Break Fix Solution</h3>
+									<p>IT OPS offers proactive event monitoring and management support services to help you respond appropriately to mitigate issues. </p>
+		
+									<div class="provided-solution">
+										<p>We provide:</p>
+		
+										<div class="grid-x grid-padding-x large-up-3 medium-up-3 small-up-1" data-equalizer="solution-item">
+											<div class="cell">
+												<div class="solution-item" data-equalizer-watch="solution-item">
+													<img draggable="false" src="./img/solution-item-1.png?v.<?php echo time();?>">
+													<p>Systems & network monitoring</p>
+												</div>
+											</div>
+		
+											<div class="cell">
+												<div class="solution-item" data-equalizer-watch="solution-item">
+													<img draggable="false" src="./img/solution-item-2.png?v.<?php echo time();?>" style="animation-delay: 0.3s;">
+													<p style="animation-delay: 0.3s;">Patch management for security</p>
+												</div>
+											</div>
+		
+											<div class="cell">
+												<div class="solution-item" data-equalizer-watch="solution-item">
+													<img draggable="false" src="./img/solution-item-3.png?v.<?php echo time();?>" style="animation-delay: 0.5s;">
+													<p style="animation-delay: 0.5s;">Remote control to track systems</p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+			
+		<section class="s1">
+			<div class="sec3-wrap">
+				<div class="our-solution" id="solutions">
+					<div class="grid-container full">
+						<div class="grid-x grid-padding-x">
+							<div class="large-12 cell">
+								<h2>What are our solutions?</h2>
+
+								<div class="our-solution-slide">
+									<div class="slide-items">
+										<div class="slider-desc">
+											<div class="solution-items-img-1"></div>
+											<h6>Complete Remote Service Capabilities </h6>
+											<p>Enterprise class monitoring, proactive management of network lifecycle, core infrastructure, related platforms and application.</p>
+										</div>
+									</div>
+
+									<div class="slide-items">
+										<div class="slider-desc">
+											<div class="solution-items-img-2"></div>
+											<h6>Incident Monitoring</h6>
+											<p>Monitor, manage and remotely resolve impacting issues.</p>
+										</div>
+									</div>
+
+									<div class="slide-items">
+										<div class="slider-desc">
+											<div class="solution-items-img-3"></div>
+											<h6>Problem Management</h6>
+											<p>Address recurring issues to prevent or minimise repetition.</p>
+										</div>
+									</div>
+
+									<div class="slide-items">
+										<div class="slider-desc">
+											<div class="solution-items-img-4"></div>
+											<h6>Performance and Capacity Monitoring</h6>
+											<p>System threshold monitoring to address service disruptions.</p>
+										</div>
+									</div>
+
+									<div class="slide-items">
+										<div class="slider-desc">
+											<div class="solution-items-img-5"></div>
+											<h6>Security Monitoring</h6>
+											<p>Life-cycle patch and software updates management to ensure secure systems.</p>
+										</div>
+									</div>
+
+									<div class="slide-items">
+										<div class="slider-desc">
+											<div class="solution-items-img-6"></div>
+											<h6>Reporting Capabilities</h6>
+											<p>Provide visibility to stay informed, keep your environment productive and secure.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="s1">
+			<div class="sec4-wrap">
+				<div class="why-choose-us">
+					<h2>Why Choose Our Proactive<br>Remote Event Monitoring & Management?</h2>
+				</div>
+			
+				<div class="reason-choosing-us">
+					<div class="grid-container full">
+						<div class="grid-x grid-padding-x large-up-4 medium-up-2 small-up-1">
+							<div class="cell" style="margin-bottom: 1rem;">
+								<div class="reason-bg">
+									<p>Enterprise-level monitoring & support extends life cycle of devices with improved performance.</p>
+								</div>
+							</div>
+			
+							<div class="cell" style="margin-bottom: 1rem;">
+								<div class="reason-bg">
+									<p>Early detection help avoid system failure with preventative maintenance.</p>
+								</div>
+							</div>
+			
+							<div class="cell" style="margin-bottom: 1rem;">
+								<div class="reason-bg">
+									<p>You can add or remove devices of any kind, in any location.</p>
+								</div>
+							</div>
+			
+							<div class="cell" style="margin-bottom: 1rem;">
+								<div class="reason-bg">
+									<p>Reduce costs with remote troubleshooting and ease of deployment.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		<section class="s1">
+			<div class="sec5-wrap">
+				<div class="let-us-help" id="contact">
+					<div class="let-us-help-bg">
+						<div class="grid-container">
+							<div class="grid-x grid-padding-x">
+								<div class="large-12 cell">
+									<div class="let-us-help-title">
+										<h2>Let Us Help You</h2>
+									</div>
+								</div>
+								<div class="large-6 medium-12 small-12 cell social-center">
+									<div class="social-icon">
+										<h5>Connect With Us</h5>
+										<ul>
+											<li><a href="javascript:void(0);"><img src="./img/mail-white.png"><p>info@itops.com</p></a></li>
+											<li><a href="javascript:void(0);"><img src="./img/whatsapp-white.png"><p>+6012-345 6789</p></a></li>
+										</ul>
+			
+										<h5>Our Office</h5>
+										<p>Persiaran Wawasan,<br>Pusat Bandar Puchong,<br>47160 Puchong,<br>Selangor,<br>Malaysia</p>
+									</div>
+								</div>
+			
+								<div class="large-6 medium-12 small-12 cell">
+									<div class="contact-us">
+										<h2>Contact Us</h2>
+			
+										<form action="contactUs.php" method="POST" name="contact">
+											<div class="form-padding">
+												<label for="username">
+													<p>Name:</p>
+													<input type="text" name="username" id="username" placeholder="ITOPS">
+												</label>
+											</div>
+			
+											<div class="form-padding">
+												<label for="email">
+													<p>Email:</p>
+													<input type="email" name="email" id="email" placeholder="INFO@ITOPS.COM">
+												</label>
+											</div>
+			
+											<div class="form-padding">
+												<label for="message">
+													<p>Message Us:</p>
+													<textarea name="message" id="message" cols="30" rows="4" placeholder="LEAVE US A MESSAGE"></textarea>
+												</label>
+											</div>
+			
+											<div class="submit">
+												<input type="submit" name="submit" id="submit" value="Submit">
+											</div>
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<footer>
+					<p>&copy; All Right Reserved</p>
+				</footer>
+			</div>
+		</section>
+	</div>
+
+    <!-- Email Success Message -->
+    <div class="reveal emailsuccess" data-deep-link="true" id="sentSuccess" data-reveal data-close-on-click="false" data-animation-in="slide-in-down" data-animation-out="slide-in-up">
+        <img draggable="false" src="./img/mail-success.png?v.<?php echo time();?>">
+        <h6>Your email is sent successfuly</h6>
+        <p>We will get back to you as soon as possible.</p>
+        
+        <button data-close type="button" onclick="closemodal()">Close</button>
+    </div>
+
+    <!-- Email Fail Message -->
+    <div class="reveal emailfail" data-deep-link="true" id="sentFail" data-reveal data-close-on-click="false" data-animation-in="slide-in-down" data-animation-out="slide-in-up">
+        <img draggable="false" src="./img/mail-fail.png?v.<?php echo time();?>">
+        <h6>Please check your form detail.</h6>
+        <p>We hope can receive your message.</p>
+        
+        <button data-close type="button" onclick="closemodal()">Close</button>
+    </div>
+
+    <script src="./js/vendor/jquery.js"></script>
+    <script src="./js/vendor/what-input.js"></script>
+    <script src="./js/vendor/foundation.min.js"></script>
+    <script src="./js/slick.min.js"></script>
+    <script src="./js/jquery.waypoints.min.js"></script>
+    <script src="./js/jquery.waypoints.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.9/fullpage.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/additional-methods.min.js"></script>
+    <script src="./js/app.js?v.<?php echo time();?>"></script>
+
+    <script>
+        function closemodal() {
+            var closeModal = document.getElementById("close-modal");
+            closeModal.classList.remove("zf-has-scroll", "is-reveal-open");
+        }
+    </script>
+
+    <script>
+        var mybutton = document.getElementById("topBtn");
+
+        window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+
+        function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+    </script>
+
+	<script>
+		new fullpage('#fullpage', {
+			autoScrolling:true
+		})
+	</script>
+</body>
+
+</html>
